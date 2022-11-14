54a8ebe7b4fcc8ccc6a89de1aa48680fb74e8e1f
Kalpshekhar Gupta <kagu@softwareag.com>
2022-11-14 11:34:36 +0000

    OpenAPI page with better visualization of documentation and added Copy to clipboard button.

--- WxSAPIntegration-UI/src/app/openapi/openapi.component.ts
+++ WxSAPIntegration-UI/src/app/openapi/openapi.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DeveloperService, OpenAPIDocumentationResponse } from '../services/developer.service';
+import { MessageService } from '../services/message.service';
 
 @Component({
   selector: 'app-openapi',
@@ -10,12 +11,19 @@ export class OpenAPIComponent implements OnInit {
 
   public openAPIContent: string = "";
 
-  constructor(private developerService: DeveloperService) { }
+  constructor(private developerService: DeveloperService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.developerService.getOpenAPIDocumentation().subscribe({ next: (resp: OpenAPIDocumentationResponse) => {
       this.openAPIContent = resp.response.content;
     }});
+  }
+
+  public copyToClipboard(inputElement: HTMLTextAreaElement): void {
+    inputElement.select();
+    document.execCommand('copy');
+    inputElement.setSelectionRange(0, 0);
+    this.messageService.successMessage("Copied to clipboard.");
   }
 
 }
